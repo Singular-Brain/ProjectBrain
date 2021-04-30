54b2bb79683e23498d1365ba0b6bb3efbb3c9f8c
Mahbodnr <mahbodnouri@gmail.com>
2021-05-01 02:23:42 +0430

    add AdjacencyMatrix functions

--- Old_Numpy_Vectorized/run.py
+++ Old_Numpy_Vectorized/run.py
@@ -1,5 +1,6 @@
 from Visualization import NetworkPanel
 from main import *
+from AdjacencyMatrix import random_connections, recurrent_layer_wise
 
 stimuli = {
         Stimulus(0.001, lambda t: 1, [0,1]),
@@ -7,7 +8,9 @@ stimuli = {
         Stimulus(0.001, lambda t: 1 * np.sin(500*t), [3])
         }
 
-G = NeuronGroup(dt = 0.001, population_size = 100, connection_chance = 0.1, total_time = 0.1, stimuli = stimuli,
+# network = random_connections(100, connection_chance = 0.1, excitatory_chance = 0.8)
+network = recurrent_layer_wise([4, 20, 20, 20, 10, 2], recurrent_connection_chance = .05, between_connection_chance = 0.8, inside_connection_chance = 0.2, excitatory_chance = 0.8)
+G = NeuronGroup(network = network, dt = 0.001, total_time = 0.1, stimuli = stimuli,
                 base_current= 1,
                 u_thresh= 1,
                 u_rest= -0,
