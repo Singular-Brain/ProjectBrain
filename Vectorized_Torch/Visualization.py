c42690495c762a0bb6986d611dd9eb9f3c7033bc
Mahbodnr <mahbodnouri@gmail.com>
2021-05-06 23:19:42 +0430

    Complete changes from numpy

--- Vectorized_Torch/Visualization.py
+++ Vectorized_Torch/Visualization.py
@@ -8,7 +8,8 @@ import bokeh
 from bokeh.io import curdoc
 from bokeh.layouts import layout
 from bokeh.models import (Button, CategoricalColorMapper, ColumnDataSource,
-                          HoverTool, Label, SingleIntervalTicker, Slider,)
+                          HoverTool, Label, SingleIntervalTicker, Slider,
+                          Arrow, NormalHead,)
 from bokeh.models import Range1d, Circle, ColumnDataSource, MultiLine
 from bokeh.plotting import from_networkx, figure
 
@@ -24,7 +25,9 @@ class NetworkPanel:
         self.graph.add_edges_from(edges)
         self.pos = self._set_pos(self.graph, self.input_neurons)
         self._update_hover_tooltips(0)
-        self.plot = figure(tooltips = self.HOVER_TOOLTIPS,
+        self.plot = figure(
+            plot_width=1000, plot_height=520,
+            tooltips = self.HOVER_TOOLTIPS,
             tools="pan,wheel_zoom,save,reset", active_scroll='wheel_zoom',
             x_range=Range1d(-.1, sorted(self.pos.values())[-1][0]+0.1),
             y_range=Range1d(0, 1), title='Neuron Group')
@@ -37,6 +40,20 @@ class NetworkPanel:
         self.button.on_click(self._animate)
         self.callback_id = None
     
+    def _update_hover_tooltips(self, timepoint):
+        if self.group.save_history:
+            neurons_potential = dict([(i[0], i[1]) for i in enumerate(self.group.potential_history[:,timepoint].cpu().numpy())])
+            nx.set_node_attributes(self.graph, name='neurons_potential', values=neurons_potential) 
+            neurons_current = dict([(i[0], i[1]) for i in enumerate(self.group.current_history[:,timepoint].cpu().numpy())])
+            nx.set_node_attributes(self.graph, name='neurons_current', values=neurons_current)
+            self.HOVER_TOOLTIPS = [ ("Neuron", "@index"),
+                                    ("Potential", '@neurons_potential'),
+                                    ("Current", '@neurons_current'),
+                                    ]
+        else:
+            self.HOVER_TOOLTIPS = [("Neuron", "@index")]
+
+
     def _set_pos(self, graph, input_neurons):
         pos = {}
         for y, neuron in enumerate(input_neurons):
@@ -59,18 +76,6 @@ class NetworkPanel:
             for y, neuron in enumerate(not_in_input_successors):
                     pos[neuron] = (x, (y+1)/(len(not_in_input_successors)+1))
         return pos
-    
-    def _update_hover_tooltips(self, timepoint):
-        if self.group.save_history:
-            neurons_potential = dict([(i[0], i[1]) for i in enumerate(self.group.potential_history[:,timepoint].detach().cpu().numpy())])
-            nx.set_node_attributes(self.graph, name='neurons_potential', values=neurons_potential) 
-            neurons_current = dict([(i[0], i[1]) for i in enumerate(self.group.current_history[:,timepoint].detach().cpu().numpy())])
-            nx.set_node_attributes(self.graph, name='neurons_current', values=neurons_current)
-            self.HOVER_TOOLTIPS = [("Neuron", "@index"),
-                                ("Potential", '@neurons_potential'),
-                                ("Current", '@neurons_current') ]
-        else:
-            self.HOVER_TOOLTIPS = [("Neuron", "@index")]
 
     def _animate_update(self):
         self.timepoint = self.slider.value + 1
@@ -94,15 +99,19 @@ class NetworkPanel:
     def _update_network(self, timepoint):
         self._update_hover_tooltips(timepoint)
         #Update Spikes
-        spiked_neurones = dict([(i[0], i[1]) for i in enumerate((self.group.spike_train[:,timepoint].detach().cpu().numpy()).astype(str))])
+        spiked_neurones = dict([(i[0], i[1]) for i in enumerate((self.group.spike_train[:,timepoint].cpu().numpy()).astype(str))])
         nx.set_node_attributes(self.graph, name='spiked_neurons', values=spiked_neurones) 
+        #
+        elements = np.nditer(self.group.weights, flags=['multi_index'])
+        excitetory_inhibitory = {(elements.multi_index[0], elements.multi_index[1]):  str(np.sign(i)) for i in elements if i !=0.0}
+        nx.set_edge_attributes(self.graph, name='excitetory_inhibitory', values=excitetory_inhibitory)
         #Add network graph from networkx
         self.network_graph = from_networkx(self.graph, self.pos, scale=10, center=(0, 0))
         #Set node size and color
         self.network_graph.node_renderer.glyph = Circle(size=15, 
-        fill_color=bokeh.transform.factor_cmap('spiked_neurons', palette=bokeh.palettes.cividis(2), factors=['False', 'True']))
+        fill_color=bokeh.transform.factor_cmap('spiked_neurons', palette=bokeh.palettes.cividis(2), factors=('False', 'True')))
         #Set edge opacity and width
-        self.network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.5, line_width=1)
+        self.network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.5, line_width=1, line_color = bokeh.transform.factor_cmap('excitetory_inhibitory', palette=['#400000', '#004000'], factors=['-1.0', '1.0']))
         #Set edge highlight colors
         self.network_graph.edge_renderer.hover_glyph = MultiLine(line_color='blue')
         #Highlight nodes and edges
@@ -112,9 +121,27 @@ class NetworkPanel:
 
     def display(self):
         self.layout = bokeh.layouts.layout([
-            [self.slider, self.button],
             [self.plot],
+            [self.slider, self.button]
         ], sizing_mode='scale_width')
         curdoc().add_root(self.layout)
         curdoc().title = "Neuron Group"
         os.system(f'cmd /c "bokeh serve --show {self.file_path}"')
+
+
+class hv_NetworkPanel:
+    import holoviews as hv
+    from holoviews import opts
+    hv.extension('bokeh')
+    #TODO: complete this class based on: http://holoviews.org/user_guide/Network_Graphs.html
+    def __init__(self,group, input_neurons, file_path):
+        self.group = group
+        self.file_path = file_path
+        self.input_neurons = input_neurons
+        rows, cols = np.where(self.group.AdjacencyMatrix == 1)
+        edges = zip(rows.tolist(), cols.tolist())
+        self.graph = nx.DiGraph()
+        self.graph.add_edges_from(edges)
+        self.pos = self.group._set_pos(self.graph, self.input_neurons)
+        plot = hv.Graph.from_networkx(self.graph, self.pos).opts(tools=['hover'])
+        hv.save(plot, 'plt.html')
