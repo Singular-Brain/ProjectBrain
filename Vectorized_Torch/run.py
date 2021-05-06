f88164ad8f6bbb3e3df48f6d557fe5ec456a0d4f
Mahbodnr <mahbodnouri@gmail.com>
2021-05-06 22:23:54 +0430

    add recurrent layerwise

--- Vectorized_Torch/run.py
+++ Vectorized_Torch/run.py
@@ -7,8 +7,8 @@ stimuli = {
         Stimulus(0.001, lambda t: 1 * np.sin(500*t), [3])
         }
 
-# based on Neural dynamics P5, each neuron connects to more than 10^4 postsynaptic neurons,
-# and we have ~ 10^11 neurons. spikes has 100 mv apmplitude, and a duration of 1-2 ms.
+# network = random_connections(100, connection_chance = 0.1, excitatory_chance = 0.8)
+network = recurrent_layer_wise([4, 20, 20, 20, 10, 2], recurrent_connection_chance = .05, between_connection_chance = 0.8, inside_connection_chance = 0.2, excitatory_chance = 0.8)
 G = NeuronGroup(dt = 0.001, population_size = 100, connection_chance = 0.1, total_time = 0.1, stimuli = stimuli,
                 base_current= 1,
                 u_thresh= 1,
