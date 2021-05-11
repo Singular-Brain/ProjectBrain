6f406d3ff26635ecb1a3b39cc6fd7ed97614ce93
Mahbodnr <mahbodnouri@gmail.com>
2021-05-11 17:02:57 +0430

    minor change

--- Vectorized_Torch/run.py
+++ Vectorized_Torch/run.py
@@ -12,7 +12,7 @@ stimuli = {
 
 network = recurrent_layer_wise([1, 3, 2], recurrent_connection_chance = .05, between_connection_chance = 0.8, inside_connection_chance = 0.2, excitatory_chance = 0.8, between_connection_chance_decay=0.85)
 
-G = NeuronGroup(network=network, dt = 0.001, population_size = 100, connection_chance = 0.1, total_time = 0.1, stimuli = stimuli,
+G = NeuronGroup(network=network, dt = 0.001, population_size = 100, total_time = 0.1, stimuli = stimuli,
                 base_current= 1,
                 u_thresh= 1,
                 u_rest= -0,
