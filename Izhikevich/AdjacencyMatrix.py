a9c9e23cfa9d5042ec83b69974c52856d90d49a1
Mahbodnr <mahbodnouri@gmail.com>
2021-05-21 13:52:56 +0430

    change recurrent_layer_wise structure

--- Izhikevich/AdjacencyMatrix.py
+++ Izhikevich/AdjacencyMatrix.py
@@ -16,24 +16,35 @@ def uniform_connections(population_size, connection_chance, excitetory_weight = 
     np.fill_diagonal(weights, 0)
     return weights
     
-def recurrent_layer_wise(layers, recurrent_connection_chance, between_connection_chance, inside_connection_chance, excitatory_chance = 0.8, between_connection_chance_decay=1.0):
+def recurrent_layer_wise(layers, recurrent_connection_chance,
+    between_connection_chance, inside_connection_chance, excitatory_ratio = 0.8,):
+    '''
+    recurrent_connection_chance: 'float'
+    between_connection_chance: 'float' or 'list of floats' 
+    inside_connection_chance: 'float' or 'list of floats'
+    excitatory_ratio: 'float' or 'list of floats'
+    '''
+    N_layers = len(layers)
+    between_connection_chance = [between_connection_chance] * N_layers if type(between_connection_chance) != list else between_connection_chance
+    inside_connection_chance = [inside_connection_chance] * N_layers if type(inside_connection_chance) != list else inside_connection_chance
+    excitatory_ratio = [excitatory_ratio] * N_layers if type(excitatory_ratio) != list else excitatory_ratio
     population_size = sum(layers)
     recurrent_weights_values = np.random.rand(population_size,population_size) * (np.random.rand(population_size, population_size) + recurrent_connection_chance).astype(np.int)
     weights =np.tril(recurrent_weights_values,-1)
     index = 0
-    between_connection_chance = between_connection_chance
-    for layer in range(len(layers)):
-        N = layers[layer]
+    excitatory_neurons = []
+    for i, (N, between_chance, inside_chance, excitatory) in enumerate(zip(layers, between_connection_chance, inside_connection_chance,excitatory_ratio)):
+        # excitatory neurons
+        excitatory_neurons += [1]* int(excitatory * N) + [-1]* (ceil((1-excitatory) * N))
         # inside layer:
-        weights_values = np.random.rand(N,N) * (np.random.rand(N, N) + inside_connection_chance).astype(np.int)
+        weights_values = np.random.rand(N,N) * (np.random.rand(N, N) + inside_chance).astype(np.int)
         np.fill_diagonal(weights_values, 0)
         weights[index:index+N, index:index+N] = weights_values
         #between layers:
-        if layer < len(layers)-1:
-            next_N = layers[layer + 1]
-            weights_values = np.random.rand(N,next_N) * (np.random.rand(N,next_N) + between_connection_chance).astype(np.int)
+        if i < len(layers)-1:
+            next_N = layers[i + 1]
+            weights_values = np.random.rand(N,next_N) * (np.random.rand(N,next_N) + between_chance).astype(np.int)
             weights[index:index+N, index+N:index+N+next_N] = weights_values
-            between_connection_chance *= between_connection_chance_decay
         index+= N  
-    excitatory_neurons = (np.random.rand(population_size,1) + excitatory_chance).astype(np.int) * 2 -1       
+    excitatory_neurons = np.array(excitatory_neurons).reshape(population_size, 1)
     return weights * excitatory_neurons
