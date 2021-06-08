ad6fccd4baee68a11adffa14c920d9219243e148
Mahbodnr <mahbodnouri@gmail.com>
2021-06-08 17:51:23 +0430

    add callbacks + add TensorBoard callback

---
+++ main/callbacks.py
@@ -0,0 +1,64 @@
+class CallbackList:
+    def __init__(self, callbacks):
+        self.callbacks = callbacks
+        for callback in callbacks:
+            assert isinstance(callback,Callback), \
+                f"All elements must be an instance of 'Callback' class.Found {callback} of type {type(callback)}"
+
+    def on_run_start(self, N_runs):
+        if self.callbacks:
+            for callback in self.callbacks:
+                callback.on_run_start(self, N_runs)
+
+    def on_timepoint_start(self, timepoint,):
+        if self.callbacks:
+            for callback in self.callbacks:
+                callback.on_run_start(self, timepoint)
+            
+    def on_run_end(self, N_runs):
+        if self.callbacks:
+            for callback in self.callbacks:
+                callback.on_run_start(self, N_runs)
+
+    def on_timepoint_end(self, timepoint):
+        if self.callbacks:
+            for callback in self.callbacks:
+                callback.on_run_start(self, timepoint)    
+
+
+class Callback:
+    def __init__(self,):
+        ...
+
+    def set_NeuronGroup(self, NeuronGroup):
+        self.model = NeuronGroup
+
+    
+    def on_run_start(self, N_runs,):
+        ...
+
+    def on_timepoint_start(self, timepoint,):
+        ...
+
+    def on_run_end(self, N_runs,):
+        ...
+
+    def on_timepoint_end(self, timepoint,):
+        ...
+    
+
+class TensorBoard(Callback):
+    def __init__(self,):
+        super().__init__()
+        from torch.utils.tensorboard import SummaryWriter
+        self.writer = SummaryWriter()
+
+    def on_run_start(self, N_runs,):
+        if N_runs ==0:
+            self.writer.add_histogram('distribution of weights', self.model.weight_values, 0)
+
+    def on_run_end(self, N_runs,):
+        self.writer.add_histogram('distribution of weights', self.model.weight_values, 1)
+    
+
+        
