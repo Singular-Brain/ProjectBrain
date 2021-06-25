23f1e4cf54b74de07083333da7b7d593579ddfe5
Mahbodnr <mahbodnouri@gmail.com>
2021-06-25 12:35:34 +0430

    enable hyper-parameters callback

--- main/callbacks.py
+++ main/callbacks.py
@@ -127,14 +127,14 @@ class TensorBoard(Callback):
 
     def on_run_start(self, N_runs,):
         # Save hyper-parameters
-        # self.writer.add_text('Network', pprint.pformat(self.network.__dict__))
-        # if self.network.learning_rule:
-        #     self.writer.add_text(type(self.network.learning_rule).__name__, 
-        #                          pprint.pformat(self.network.learning_rule.__dict__))
-        # for group in self.network.groups:
-        #     self.writer.add_text(group.name, pprint.pformat(group.__dict__) + pprint.pformat(group.neuronType.__dict__))
-        # for connection in self.network.connections:
-        #     self.writer.add_text(connection.name, pprint.pformat(connection.__dict__))
+        self.writer.add_text('Network', pprint.pformat(self.network.__dict__))
+        if self.network.learning_rule:
+            self.writer.add_text(type(self.network.learning_rule).__name__, 
+                                 pprint.pformat(self.network.learning_rule.__dict__))
+        for group in self.network.groups:
+            self.writer.add_text(group.name, pprint.pformat(group.__dict__) + pprint.pformat(group.neuronType.__dict__))
+        for connection in self.network.connections:
+            self.writer.add_text(connection.name, pprint.pformat(connection.__dict__))
         # Initial state
         if self.update_secs:
             self.step =int(self.update_secs/self.network.dt)
